8e3212e469a638678b874470e26d6ec473fcc475
Stu Salsbury <stu.salsbury@gmail.com>
2014-06-25 14:35:48 -0700

    style: begin to semanticize
    Begin to:
    
    * make all of our html semantic through html5 tags and .ss-
    prefixed-classes;
    * map our semantics to bootstrap;
    * override bootstrap to our geometry and wireframy styling.
    
    In addition:
    
    * restore source maps for sass.

--- browser/src/app/states/_root.js
+++ browser/src/app/states/_root.js
@@ -18,8 +18,8 @@ define(['app/module'], function (module) {
       // todo -- there is no real reason that we should be
       // doing it this way -- it is sloppy to use jqLite outside of
       // directives -- should set scope property
-      var htmlElement = window.angular.element(document.children[0]);
-      htmlElement.addClass('spun');
+      // var htmlElement = window.angular.element(document.children[0]);
+      // htmlElement.addClass('spun');
     }
 
   ]);
