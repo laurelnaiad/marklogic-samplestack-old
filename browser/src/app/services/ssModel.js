9ab01748fa57f0432cbf049a4e0f4492fd37c362
Stu Salsbury <stu.salsbury@gmail.com>
2014-07-07 01:50:21 -0700

    begin ssModel

--- browser/src/app/services/ssModel.js
+++ browser/src/app/services/ssModel.js
@@ -4,9 +4,33 @@ define(['app/module'], function (module) {
 
     'mlModel',
     function (mlModel) {
-      return {
-        tweedle: 'dee'
 
+      var Tag = function (spec) {
+
+        _.merge(this, spec);
+
+        mlModel.call(this);
+      };
+
+      Tag.prototype = Object.create(mlModel.prototype, {
+      });
+
+      //static functions
+      _.merge(Tag, mlModel, {
+        create: function (spec) {
+          return new Tag(spec);
+        },
+        find: function (spec) {
+          return [
+            Tag.create( { name: 'tag1', count: 6 } ),
+            Tag.create( { name: 'tag2', count: 2 } )
+          ];
+        }
+      });
+
+      return {
+
+        tag: Tag
       };
     }
 
