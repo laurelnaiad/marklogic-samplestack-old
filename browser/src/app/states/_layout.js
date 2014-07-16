ee799f11c02ba52b9c615068a10ef18d5204c288
Mike Wooldridge <mike@hellion.org>
2014-07-16 10:20:27 -0700

    browser/miscellaneous: contributor popup, ask navigation

--- browser/src/app/states/_layout.js
+++ browser/src/app/states/_layout.js
@@ -4,18 +4,12 @@ app/states/_layout.js
 define(['app/module'], function (module) {
 
   module.controller('layoutCtlr', [
-
     '$scope', 'appRouting',
     function ($scope, appRouting) {
-      // TODO: this is dead code, do we need a controller?
-      // $scope.collapsed = true;
-
-
       $scope.ask = function () {
         appRouting.go('ask');
       };
     }
 
   ]);
-
 });
