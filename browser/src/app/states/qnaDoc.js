02006edd03845b72392134c61fbae501422ffabf
Mike Wooldridge <mike@hellion.org>
2014-07-27 16:26:39 -0700

    browser/qnadoc: add styled vote box, start answer sorting
    Conflicts:
    	browser/src/app/styles/style.scss

--- browser/src/app/states/qnaDoc.js
+++ browser/src/app/states/qnaDoc.js
@@ -5,6 +5,29 @@ define(['app/module'], function (module) {
     '$scope',
     function ($scope) {
       $scope.setPageTitle('doc');
+
+      // Sort settings
+      $scope.sorts = [
+        {
+          label: 'newest',
+          value: ['creationDate']
+        },
+        {
+          label: 'votes',
+          value: ['']
+        }
+      ];
+      $scope.selectedSort = $scope.sorts[1]; // Default sort
+
+     /*
+      * Set the tag sort based on clicked element.
+      */
+      $scope.setSort = function () {
+        $scope.selectedSort = this.sort;
+        console.log('sort: ' + this.sort.label);
+        // emit()
+      };
+
     }
 
   ]);
